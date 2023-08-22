3b10d796dcc9e5987fc6e3884173b7cf15ba0858
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2023-08-22 10:26:08 +0200

    Added dotenv

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 console.log("The app is running");
-
-const SECRET_KEY = "password303949234";
-
-process.env.SECRET_KEY = SECRET_KEY;
-process.env.PASSWORD = "asdaldklsad";
-process.env.HOST = "192.168.1.2";
-
-console.log(process.env);
+console.log(process.env.PASSWORD);
